b5fb8e9e871cbcf0b2d8202ec80d434434efa716
Theodore Nordsieck <theodore.nordsieck@gmail.com>
2014-05-12 17:33:25 -0700

    Added failing test

--- main.go
+++ main.go
@@ -1,5 +1,7 @@
 package main
 
+import "fmt"
+
 func main() {
-	panic("Error!")
+	fmt.Println("Hello, World!")
 }
---
+++ main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestMainA(t *testing.T) {
+	t.Error("Test Fails")
+}
+
+func TestMainB(t *testing.T) {
+	//fn passes
+}
